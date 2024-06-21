59bbbf251532da46edb36dce4914acf4cbe42bae
Nethz Jimenez <bynethz@gmail.com>
2024-06-21 13:48:17 -0500

    feat: Add file helpers, update gitignore, and add product image entity

---
+++ src/messages-ws/messages-ws.gateway.ts
@@ -0,0 +1,64 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { MessagesWsService } from './messages-ws.service';
+import { Socket } from 'socket.io';
+import { NewMessageDto } from './dtos/new-message.dto';
+import { JwtService } from '@nestjs/jwt';
+import { JwtPayload } from 'src/auth/interfaces';
+
+@WebSocketGateway({ cors: true })
+export class MessagesWsGateway
+  implements OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() wss: Socket;
+  constructor(
+    private readonly messagesWsService: MessagesWsService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async handleConnection(client: Socket) {
+    const token = client.handshake.headers.token as string;
+    let payload: JwtPayload;
+
+    try {
+      payload = this.jwtService.verify(token);
+      await this.messagesWsService.registerClient(client, payload.id);
+    } catch (error) {
+      client.disconnect();
+      return;
+    }
+
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+
+  handleDisconnect(client: Socket) {
+    this.messagesWsService.removeClient(client.id);
+
+    this.wss.emit(
+      'clients-updated',
+      this.messagesWsService.getConnectedClients(),
+    );
+  }
+  @SubscribeMessage('new-message-from-client')
+  handleMessageFromClient(client: Socket, payload: NewMessageDto) {
+    // You can send the message to all clients
+    this.wss.emit('new-message-from-server', {
+      fullName: this.messagesWsService.getUserFullName(client.id),
+      message: payload,
+    });
+
+    // Alternatively, you can send the message to self client
+    //client.emit('new-message-from-server', payload);
+
+    // Alternatively, you can send the message to everybody except self client
+    //client.broadcast.emit('new-message-from-server', payload);
+  }
+}
